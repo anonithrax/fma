44560e85e486719a0600f5ea8b70ebaf2f9e802c
AIHxDev <108882092+aihxdev@users.noreply.github.com>
2024-04-02 06:12:45 +0530

    removed comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var digit = 1; // Default digit count
-var question = 5; // Default number of questions
-var speed = 0.5; // Default speed in seconds
+var digit = 1;
+var question = 5;
+var speed = 0.5;
 var numbers = [];
 var currentQuestionIndex = 0;
 var startTime;
@@ -8,14 +8,12 @@ var endTime;
 var score = 0;
 
 function startGame() {
-    // Hide settings and show game screen
+
     document.getElementById("settings").style.display = "none";
     document.getElementById("game").style.display = "block";
 
-    // Generate numbers
     generateNumbers();
 
-    // Start displaying numbers
     displayNumbers();
 }
 
@@ -24,40 +22,36 @@ function generateNumbers() {
     for (var i = 0; i < question; i++) {
         var num = "";
         for (var j = 0; j < digit; j++) {
-            num += Math.floor(Math.random() * 10); // Generate random digit
+            num += Math.floor(Math.random() * 10);
         }
-        numbers.push(parseInt(num)); // Convert string to integer for sum calculation
+        numbers.push(parseInt(num));
     }
 }
 
 function displayNumbers() {
     var currentIndex = 0;
 
-    var intervalId = setInterval(function() {
+    var intervalId = setInterval(function () {
         if (currentIndex < numbers.length) {
             document.getElementById("number").innerText = numbers[currentIndex++];
         } else {
-            clearInterval(intervalId); // Stop the interval
+            clearInterval(intervalId);
             promptUser();
         }
-    }, speed * 1000); // Convert seconds to milliseconds
+    }, speed * 1000);
 }
 
 function promptUser() {
-    // Prompt user for the sum of the displayed numbers
     var userSum = parseInt(prompt("Enter the sum of the displayed numbers:"));
 
-    // Calculate the actual sum of the numbers
-    var actualSum = numbers.reduce(function(acc, curr) {
+    var actualSum = numbers.reduce(function (acc, curr) {
         return acc + curr;
     }, 0);
 
-    // Check if the user's input matches the actual sum
     if (userSum === actualSum) {
-        score += 10; // Increment score for correct answer
+        score += 10;
     }
 
-    // End the game
     endGame();
 }
 
